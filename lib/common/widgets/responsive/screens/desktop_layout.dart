b68673984fa4adf168eee5588cdc2567b1750c2d
Drogbut <tchioidriss@yahoo.fr>
2024-11-13 19:06:45 +0100

    - Add adaptive appbar - Toogle menu icon in mobile and tablet

--- lib/common/widgets/responsive/screens/desktop_layout.dart
+++ lib/common/widgets/responsive/screens/desktop_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../containers/rounded_container.dart';
+import '../../layout/headers/header.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key, this.body});
@@ -25,12 +25,7 @@ class DesktopLayout extends StatelessWidget {
               child: Column(
                 children: [
                   /// HEADER
-                  TRoundedContainer(
-                    width: double.infinity,
-                    height: 75,
-                    backgroundColor: Colors.yellow.withOpacity(0.2),
-                    child: Center(child: Text('Desktop appbar')),
-                  ),
+                  THeader(),
 
                   /// BODY
                   body ?? Center(child: Text('desktop body')),
