d850eaa706e076e6d9ff083de25f8ee8b422df7a
Drogbut <tchioidriss@yahoo.fr>
2024-11-18 21:07:33 +0100

    09 - Redirect an unlogged-in user to the login page using GetX

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,13 +3,16 @@ import 'package:get/get.dart';
 import '../modules/authentication/forget_password/presenter/ui/forget_password.dart';
 import '../modules/authentication/login/presenter/ui/login.dart';
 import '../modules/authentication/reset_password/presenter/ui/reset_password.dart';
+import '../modules/dashboard/presenter/ui/dashboard_screen.dart';
 import 'route_middleware.dart';
 import 'routes.dart';
 
 class TAppRoute {
   static List<GetPage> getPages = [
-    GetPage(name: TRoutes.login, page: () => TLoginPage(), middlewares: [RouteMiddleWare()]),
+    GetPage(name: TRoutes.login, page: () => LoginScreen()),
     GetPage(name: TRoutes.forgetPassword, page: () => ForgotPasswordScreen()),
     GetPage(name: TRoutes.resetPassword, page: () => ResetPasswordScreen()),
+    GetPage(name: TRoutes.resetPassword, page: () => ResetPasswordScreen()),
+    GetPage(name: TRoutes.dashboard, page: () => DashboardScreen(), middlewares: [RouteMiddleWare()]),
   ];
 }
